40529c18ff9ad5f541e1e312e5fafbda684695e1
KiwiKid <KiwiKid@users.noreply.github.com>
2022-10-25 22:52:59 +1300

    new data + update script

--- scripts/update_data.sh
+++ scripts/update_data.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+# (need permissions?) - run: chmod ug+x update-data.sh
 
-# https://stackoverflow.com/a/20434740
+# https://stackoverflow.com/a/20434740 (get the current directory)
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-# (need permissions?) - chmod ug+x update-data.sh
 
-curl 'http://localhost:3000/api/property' >> "$DIR/../pages/data/test_data_new.json"
+cp "$DIR/../pages/data/test_data.json" "$DIR/../pages/data/test_data_previous.json" 
 
-echo "new file here: $DIR/../pages/data/test_data_new.json"
+curl 'http://localhost:3000/api/property' >> "$DIR/../pages/data/test_data.json"
 
-echo "Rename to \"test_data.json\" to apply"
+
+echo "new file here: $DIR/../pages/data/test_data.json"
+echo "(old file here: $DIR/../pages/data/test_data_previous.json)"
+
+echo "Commit new \"test_data.json\" to apply"
