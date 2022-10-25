6102cefeb01cc560c5959d1bd635fd06a9f929f4
KiwiKid <KiwiKid@users.noreply.github.com>
2022-10-26 00:20:34 +1300

    only show worst addresses

--- scripts/update_data.sh
+++ scripts/update_data.sh
@@ -4,7 +4,7 @@
 # https://stackoverflow.com/a/20434740 (get the current directory)
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-cp "$DIR/../pages/data/test_data.json" "$DIR/../pages/data/test_data_previous.json" 
+mv "$DIR/../pages/data/test_data.json" "$DIR/../pages/data/test_data_previous.json" 
 
 curl 'http://localhost:3000/api/property' >> "$DIR/../pages/data/test_data.json"
 
