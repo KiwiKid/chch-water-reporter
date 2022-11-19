a1c99b0f351670f69d714c749b58e414c548393d
KiwiKid <KiwiKid@users.noreply.github.com>
2022-11-19 21:21:20 +1300

    better verification

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Christchurch Water Usage</title>
         <meta name="description" content="Christchurch water usage visualised" />
+        <meta name="google-site-verification" content="n16jJwjABkgf8P-Js7ofLvyXSdA4Y7jdMGytnGoGIM0" />
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💧</text></svg>">
         </link>
       </Head>
