e7f81eec18e096053c2e8a542ff6c3d818479989
KiwiKid <KiwiKid@users.noreply.github.com>
2022-11-24 10:20:23 +1300

    fix tpye check

--- components/PropertyCircleMarker.tsx
+++ components/PropertyCircleMarker.tsx
@@ -17,37 +17,39 @@ export default function PropertyCircleMarker({p}:PropertyCircleMarkerProps) {
   const MIN_CIRCLE_SIZE = 0;
   const MAX_CIRCLE_SIZE = 5000;
   const getCircleSize = (p:PropertyWithUsages) => {
-    let scaleFactor = 0.000
-    let zoom = mapEvents.getZoom()
-    // (hacky prevent lots of recalc when the zoom changes)
-    if(!zoomTracker || zoom != zoomTracker){
-      setZoomTracker(zoomTracker)
-      if(zoom === 18){
-        scaleFactor = 0.0014
-      }else if(zoom >= 16){
-        scaleFactor = 0.0012
-      }else if(zoom >= 14){
-        scaleFactor = 0.0010
-      }else if(zoom >= 10){
-        scaleFactor = 0.0008
-      }else if(zoom >= 8){ 
-        scaleFactor = 0.0005
-      }else if(zoom >= 6){
-        scaleFactor = 0.0007
-      }else{
-        scaleFactor = 0.0001
-      }
-      return Math.min(Math.max((p.averageUsage/2), MIN_CIRCLE_SIZE), MAX_CIRCLE_SIZE)*(scaleFactor*zoom)
+  let scaleFactor = 0.000
+  let zoom = mapEvents.getZoom()
+  // (hacky prevent lots of recalc when the zoom changes)
+    if(zoom === 18){
+      scaleFactor = 0.0014
+    }else if(zoom >= 16){
+      scaleFactor = 0.0012
+    }else if(zoom >= 14){
+      scaleFactor = 0.0010
+    }else if(zoom >= 10){
+      scaleFactor = 0.0008
+    }else if(zoom >= 8){ 
+      scaleFactor = 0.0005
+    }else if(zoom >= 6){
+      scaleFactor = 0.0007
+    }else{
+      scaleFactor = 0.0001
     }
+    return Math.min(Math.max((p.averageUsage/2), MIN_CIRCLE_SIZE), MAX_CIRCLE_SIZE)*(scaleFactor*zoom)
   }
 
   useEffect(() => {
-    setCircleSize(getCircleSize(p))
+    if(!zoomTracker || zoom != zoomTracker){
+      setZoomTracker(zoomTracker)
+      setCircleSize(getCircleSize(p))
+    }
   }, [])
   
   const mapEvents = useMapEvents({
       zoomend: () => {
-        setCircleSize(getCircleSize(p))
+        if(!zoomTracker || zoom != zoomTracker){
+          setCircleSize(getCircleSize(p))
+        }
       },
   });
 
