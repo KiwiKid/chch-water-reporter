808ae5dc973f1c03cb43ddf5bf768035706c4549
KiwiKid <KiwiKid@users.noreply.github.com>
2022-11-23 14:36:46 +1300

    improve title

--- pages/graphs.tsx
+++ pages/graphs.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <Head>
         <title>Christchurch Water Usage</title>
-        <meta name="description" content="Christchurch water usage visualised" />
+        <meta name="description" content="Christchurch Water Usage - Visualised" />
         <meta name="og:image" content="/preview.png"/>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💧</text></svg>">
         </link>
