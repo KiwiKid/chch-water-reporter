40d005c0e695d90a94e1420840afdfa873243506
KiwiKid <KiwiKid@users.noreply.github.com>
2022-11-23 14:24:53 +1300

    fix og:image tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
         <title>Christchurch Water Usage</title>
         <meta name="description" content="Christchurch water usage visualised" />
         <meta name="google-site-verification" content="n16jJwjABkgf8P-Js7ofLvyXSdA4Y7jdMGytnGoGIM0" />
-        <meta property="og:image" content="/preview.png"/>
+        <meta name="og:image" content="/preview.png"/>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💧</text></svg>">
         </link>
       </Head>
