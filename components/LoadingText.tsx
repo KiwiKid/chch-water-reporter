f47b0e8d6f95e70e0ad7743d091cc540e92a388f
KiwiKid <KiwiKid@users.noreply.github.com>
2022-12-22 10:10:46 +1300

    copy update

--- components/LoadingText.tsx
+++ components/LoadingText.tsx
@@ -1,6 +1,6 @@
 const LoadingText = () => {
     return (
-        <><h1>Loading...</h1><h2>(Zoom in to see more properties)</h2></>
+        <><h1>Loading...</h1><h2>(Zoom in to see more circles)</h2></>
     )
 }
 
